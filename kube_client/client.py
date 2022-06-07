b4ef40f54cca6fa03cfd7908df90e2b672a65986
alirezaSafi <ali02reza78@gmai.com>
2022-06-07 18:19:27 +0430

    Handle exception errors

--- kube_client/client.py
+++ kube_client/client.py
@@ -1,4 +1,6 @@
 from kubernetes import client
+from kubernetes.client.exceptions import ApiException
+from rest_framework.exceptions import APIException
 
 
 class KubeClient:
@@ -11,12 +13,27 @@ class KubeClient:
     def apps_v1_client(configuration):
         return client.AppsV1Api(client.ApiClient(configuration))
 
-    def list(self, namespace, resource_obj, configuration):
-        """use apps-v1-client to to get the list of resource objects."""
+    @staticmethod
+    def _make_func_call(api_client, func, **kwargs):
+        try:
+            response = getattr(
+                api_client,
+                func
+            )(**kwargs)
+            return response
+        except ApiException as e:
+            raise APIException(detail=e.reason, code=e.status)
+
+    def list(self, namespace, selectors, resource_obj, configuration):
+        """use apps-v1-client to get the list of resource objects."""
         if resource_obj not in self.apps_v1_list_func_mapping:
             raise ValueError("the resource object isn't valid!")
-        response = getattr(
+        func_args = {
+            "namespace": namespace,
+            **selectors
+        }
+        return self._make_func_call(
             self.apps_v1_client(configuration),
-            self.apps_v1_list_func_mapping[resource_obj]
-        )(namespace=namespace)
-        return response
+            self.apps_v1_list_func_mapping[resource_obj],
+            **func_args
+        )
