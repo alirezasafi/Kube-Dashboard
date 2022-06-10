e46d9f032dc272fef0a921dd405b0d75bd262ea3
alirezaSafi <ali02reza78@gmai.com>
2022-06-10 17:52:28 +0430

    Fix default authentication class

--- authentication/authentication.py
+++ authentication/authentication.py
@@ -1,12 +1,13 @@
+from typing import Any, Tuple, Union
 from rest_framework.authentication import SessionAuthentication
-from kube_client.config import KubeConfiguration
+from kube_client.config import KubeConfiguration, Configuration
 
 
 class KubeAuthentication(SessionAuthentication):
 
-    def authenticate(self, request):
+    def authenticate(self, request) -> Tuple[Any, Union[Any, Configuration]]:
         token = request.session.get("token")
         if not token:
-            return None
+            return None, None
         return None, KubeConfiguration.token_configuration(token=token)
 
--- authentication/permission.py
+++ authentication/permission.py
@@ -1,7 +1,8 @@
 from rest_framework.permissions import BasePermission
+from kube_client.config import Configuration
 
 
 class IsAuthenticated(BasePermission):
 
-    def has_permission(self, request, view):
-        return bool(request.auth)
+    def has_permission(self, request, view) -> bool:
+        return bool(isinstance(request.auth, Configuration))
