7ea07c814350c7da51b31bd7e970acb492123bca
alirezaSafi <ali02reza78@gmai.com>
2022-06-11 16:20:11 +0430

    Add ws-authentication class

--- authentication/permission.py
+++ authentication/permission.py
@@ -1,4 +1,7 @@
+from typing import Dict, Any
 from rest_framework.permissions import BasePermission
+from djangochannelsrestframework.permissions import BasePermission as WSBasePermission
+from channels.consumer import AsyncConsumer
 from kube_client.config import Configuration
 
 
@@ -6,3 +9,10 @@ class IsAuthenticated(BasePermission):
 
     def has_permission(self, request, view) -> bool:
         return bool(isinstance(request.auth, Configuration))
+
+
+class WSIsAuthenticated(WSBasePermission):
+    async def has_permission(
+        self, scope: Dict[str, Any], consumer: AsyncConsumer, action: str, **kwargs
+    ) -> bool:
+        return True
